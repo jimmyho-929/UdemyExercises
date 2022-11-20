349fe3da970262e736a6d27fcbcea2c49308f379
jimmy.ho <jimmyho.929@outlook.com>
2022-11-19 23:28:46 -0600

    refactoring loop exercises

--- src/MyNumber.java
+++ src/MyNumber.java
@@ -1,22 +1,34 @@
 public class MyNumber {
-    public boolean isPrime(int value){
-        if( value %3==0){
-            return false;
-        } else if (value%2==0) {
-            return false;
-        } else return true;
+    private int num;
+    public MyNumber(int num){
+        this.num = num;
     }
 
-//    public int sumUpToN() {
-//
-//    }
-//
+    public boolean isPrime(){
+        for (int i = 2; i<=this.num - 1;i++) {
+            if (this.num % i == 0) return false;
+        }
+        return true;
+    }
+
+    public int sumUpToN() {
+        int sum = 0;
+        for(int i=1;i<=this.num;i++){
+            sum += i;
+        }
+        return sum;
+    }
+
 //    public int sumOfDivisors(){
 //
 //    }
 //
-//    public void printANumberTriangle(){
-//
-//    }
+    public void printANumberTriangle(){
+        String numTriangle = "";
+        for (int i = 1; i<=this.num;i++) {
+            numTriangle += " " + i;
+            System.out.println(numTriangle);
+        }
+    }
 
 }
---
+++ src/MyNumberRunner.java
@@ -0,0 +1,8 @@
+public class MyNumberRunner {
+    public static void main(String[] args) {
+        MyNumber number = new MyNumber(3);
+
+
+        System.out.println(number.isPrime());
+    }
+}
