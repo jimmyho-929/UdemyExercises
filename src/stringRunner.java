b818e16fd0e4ae13c87347e01b6e25118c912616
jimmy.ho <jimmyho.929@outlook.com>
2022-11-28 23:28:10 -0600

    print string char loop

---
+++ src/stringRunner.java
@@ -0,0 +1,10 @@
+public class stringRunner {
+    public static void main(String[] args) {
+        String someString = "This is a lot of text again";
+
+        for (int i =0; i < someString.length() ;i++) {
+            System.out.println(someString.charAt(i));
+        }
+
+    }
+}
