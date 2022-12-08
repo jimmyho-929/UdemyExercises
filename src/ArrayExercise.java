61eb91d12545d6ab5049337a1b303d15af31b264
jimmy.ho <jimmyho.929@outlook.com>
2022-12-07 21:33:32 -0600

    array exercise solution. introduced to ArrayList

--- src/ArrayExercise.java
+++ src/ArrayExercise.java
@@ -1,21 +1,17 @@
 public class ArrayExercise {
     public static void main(String[] args) {
         String[] weekdays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
-        int longestString = 0;
-        String longestDay = null;
+        String longestDay = "";
 
         for (String day:weekdays) {
-            for (int i = 0; i <= weekdays.length-1; i++) {
-                if (weekdays[i].length() > longestString) {
-                    longestString = weekdays[i].length();
-                    longestDay = weekdays[i];
-                }
-            }
+           if (day.length() > longestDay.length()) {
+               longestDay = day;
+           }
         }
 
         System.out.println(longestDay);
 
-        for (int i = 6; i >=0; i--) {
+        for (int i = weekdays.length -1; i >=0; i--) {
             System.out.println(weekdays[i]);
             }
         }
