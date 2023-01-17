e772468061c4f7d4045baac771d2ef9a659a7f94
jimmy.ho <jimmyho.929@outlook.com>
2023-01-16 21:30:38 -0600

    linked set example

--- src/ListExample.java
+++ src/ListExample.java
@@ -1,10 +1,29 @@
 import java.util.ArrayList;
+import java.util.LinkedHashSet;
 import java.util.List;
+import java.util.TreeSet;
 
 public class ListExample {
     public static void main(String[] args) {
-        List<String> words = new ArrayList<>();
-        words.add(0,"Hello World");
-        System.out.println(words.contains("Hello World"));
+
+        //store array of char [A,Z,A,B,Z,F]
+        List<Character> characters = new ArrayList<>();
+        characters.add('A');
+        characters.add('Z');
+        characters.add('A');
+        characters.add('B');
+        characters.add('Z');
+        characters.add('F');
+
+        System.out.println(characters);
+        //create TreeSet
+
+        TreeSet<Character> characterTreeSet= new TreeSet<>(characters);
+        System.out.println(characterTreeSet);
+
+        //create LinkedHashSet
+
+        LinkedHashSet<Character> characterLinkedHashSet = new LinkedHashSet<>(characters);
+        System.out.println(characterLinkedHashSet);
     }
 }
